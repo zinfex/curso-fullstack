88df6dd0a64ef291c42317f45a529d331807e624
Souzaa <122699226+zinfex@users.noreply.github.com>
2023-11-27 00:58:14 -0300

    Add files via upload

---
+++ AULA_32_NODEJS/models/UsuariosModel.js
@@ -0,0 +1,46 @@
+import Conexao from '../config/Conexao.js';
+
+class UsuariosModel {
+
+    async findAll() {
+        const con = new Conexao();
+        const sql = "SELECT * FROM usuarios";
+        const dados = {};
+        const resultados = await con.query(sql, dados);
+        return resultados;
+    }
+
+    async findOne(id) {
+        const con = new Conexao();
+        const sql = "SELECT * FROM usuarios WHERE id = "+id;
+        const dados = {};
+        const resultados = await con.query(sql, dados);
+        return resultados;
+    }
+
+    async create(data) {
+        const con = new Conexao();
+        const sql = "INSERT INTO usuarios SET ";
+        const dados = data;
+        const resultados = await con.query(sql, dados);
+        return resultados;
+    }
+
+    async update(data, id) {
+        const con = new Conexao();
+        const sql = "UPDATE usuarios SET ? WHERE id = ? ";
+        const dados = [data, id];
+        const resultados = await con.query(sql, dados);
+        return resultados;
+    }
+
+    async delete(id) {
+        const con = new Conexao();
+        const sql = "DELETE FROM usuarios WHERE id = " +id;
+        const dados = {};
+        const resultados = await con.query(sql, dados);
+        return resultados;
+    }
+}
+
+export default UsuariosModel;
