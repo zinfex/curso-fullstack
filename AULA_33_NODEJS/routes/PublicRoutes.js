09b38eef07b9be3229cbea33535a53958d6bf499
Souzaa <122699226+zinfex@users.noreply.github.com>
2023-12-05 01:22:27 -0300

    Delete AULA_33_NODEJS directory

--- AULA_33_NODEJS/routes/PublicRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import UsuariosController from '../controllers/UsuariosController.js';
-
-const PublicRoutes = express.Router();
-
-const usuariosController = new UsuariosController();
-PublicRoutes.post('/login', usuariosController.login);
-
-export default PublicRoutes;
